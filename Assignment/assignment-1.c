9a7f64459a2d7f04d3a67d286455a2d2d89f6ec1
Muhammad UI <muhammadbmw69@gmail.com>
2021-02-23 15:40:23 +0100

    added .gitignore file and did assignment-1

---
+++ Assignment/assignment-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Assignment 1 Question
+    /*
+    1. Install Codeblocks on your computer and run it. [View ReadMe.File]
+    2. Print your name, Registration number and email.
+    */
+
+    //  Solution 1.
+    /*
+
+     If you want to know how to install Codeblocks on your computer,
+     then readme.md file on this repository
+
+    */
+
+    // if you want to print something to the screen, use the printf() function
+
+    printf('Name: Muhammad Ibrahim Umar');
+    printf('Registration Number: 16/44976u/1');
+    printf('Email: imumar117@gmail.com');
+    return 0;
+}
